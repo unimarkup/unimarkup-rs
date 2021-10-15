c6bedeacedb720476d63944813a9c9021675ee2b
Nadir Fejzic <nadirfejzo@gmail.com>
2021-10-16 01:39:57 +0200

    Implement basic parsing as proof of concept
    Note: This is just a proof of concept, refactoring needed. In
    particular, the parsing should be modified in such a way that the input
    into function is a slice (vector) reference containing lines of string.
    This is needed in order to support multi-line headings.
    
    Also, multi-line headings support must be implemented.

--- src/frontend/parser.rs
+++ src/frontend/parser.rs
@@ -1,3 +1,34 @@
+use super::blocks::heading_block::{HeadingBlock, HeadingLevel};
+
 pub fn test_function() {
     println!("Basic setup");
+}
+
+pub fn parse_heading(input: &[&str], index: &mut usize) -> HeadingBlock {
+    let mut content: String = "".into();
+
+    let mut level = HeadingLevel::Invalid;
+    let mut level_counter = 0;
+    let mut level_set = false;
+
+    while let Some(word) = input.get(*index) {
+        if word.matches('#').count() > 0 && !level_set {
+            level_counter += 1;
+        } else {
+            level_set = true;
+            level = HeadingLevel::from(level_counter);
+        }
+
+        if level_set {
+            // read content
+            content.push_str(word);
+        }
+
+        *index += 1;
+    }
+
+    HeadingBlock {
+        level,
+        content: content.trim().to_string(),
+    }
 }
--- tests/frontend/parser.rs
+++ tests/frontend/parser.rs
@@ -1,4 +1,40 @@
+use unicode_segmentation::UnicodeSegmentation;
+use unimarkup_rs::frontend::{blocks::heading_block::HeadingLevel, parser};
+
 #[test]
-fn test_something() {
-    assert_ne!(true, false);
+fn parse_heading_level1() {
+    let input = "# Heading level 1"
+        .split_word_bounds()
+        .collect::<Vec<&str>>();
+
+    let output_block = parser::parse_heading(&input, &mut 0);
+
+    assert_eq!(output_block.level, HeadingLevel::Level1);
+    assert_eq!(output_block.content, String::from("Heading level 1"));
+}
+
+#[test]
+fn parse_heading_level2() {
+    let input = "## Heading level 2"
+        .split_word_bounds()
+        .collect::<Vec<&str>>();
+
+    println!("{:?}", input);
+
+    let output_block = parser::parse_heading(&input, &mut 0);
+
+    assert_eq!(output_block.level, HeadingLevel::Level2);
+    assert_eq!(output_block.content, String::from("Heading level 2"));
+}
+
+#[test]
+fn parse_heading_level3() {
+    let input = "### Heading level 3"
+        .split_word_bounds()
+        .collect::<Vec<&str>>();
+
+    let output_block = parser::parse_heading(&input, &mut 0);
+
+    assert_eq!(output_block.level, HeadingLevel::Level3);
+    assert_eq!(output_block.content, String::from("Heading level 3"));
 }
