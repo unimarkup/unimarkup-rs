b000514b78571b644c6f1efae7f536d795693ae9
Nadir Fejzic <nadirfejzo@gmail.com>
2021-10-31 22:49:28 +0100

    Add function for counting symbols to parser.rs
    Function `count_symbol_until` is added to `parser.rs`.
    This function counts the repetitions of wanted symbol from beginning of
    the line, and checks if the symbol directly after the sequence matches
    the given constrained.
    The constraints are checked by providing a function as a parameter to
    this function. The provided function performs wanted checks, i.e. one
    such function is `char::is_whitespace(self: char)` which takes a
    character and checks if it's whitespace, as defined by unicode spec.

--- src/frontend/parser.rs
+++ src/frontend/parser.rs
@@ -1,3 +1,5 @@
+use unicode_segmentation::UnicodeSegmentation;
+
 /// CursorPos struct is used to keep track of the cursor position while parsing input.
 /// Additionally it is useful as a way to show better error messages. Using CursorPos it
 /// is possible to show exact location in the input, where parsing error is recognized.
@@ -11,4 +13,34 @@ pub struct CursorPos {
 
 pub fn is_blank_line(line: &str) -> bool {
     line.trim().is_empty()
+}
+
+pub fn count_symbol_until(
+    line: &str,
+    symbol: &str,
+    until: fn(char) -> bool,
+) -> Result<usize, (usize, String)> {
+    let mut symbols = line.graphemes(true);
+
+    let count = symbols
+        .by_ref()
+        .peekable()
+        .take_while(|&current_symbol| current_symbol == symbol)
+        .count();
+
+    let error_message = "Unexpected symbol found!";
+
+    if let Some(symbol) = line.graphemes(true).nth(count) {
+        if symbol.contains(until) {
+            Ok(count)
+        } else {
+            let message = String::from(error_message);
+
+            Err((count, message))
+        }
+    } else {
+        let message = String::from(error_message);
+
+        Err((count, message))
+    }
 }
