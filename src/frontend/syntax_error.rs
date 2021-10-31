9ea34b479373acfced030693c7ee39f3e57f0cd5
Nadir Fejzic <nadirfejzo@gmail.com>
2021-10-31 22:52:24 +0100

    Refactor UmSyntaxError for better error output
    `message` field is added to UmSyntaxError for providing more concrete
    message why parsing fails, i.e. unexpected symbol.
    
    One helper function is added to UmSyntaxError, which should help reduce
    some boilerplate code.
    
    Finally, UmSyntaxError output is modified for prettier formatting when
    outputing the (multi-line) message.

--- src/frontend/syntax_error.rs
+++ src/frontend/syntax_error.rs
@@ -9,6 +9,7 @@ pub struct UmSyntaxError {
     pub current_pos: CursorPos,
     pub start_line: String,
     pub current_line: String,
+    pub message: String,
 }
 
 impl UmSyntaxError {
@@ -31,6 +32,23 @@ impl UmSyntaxError {
 
         (String::from(start_line), String::from(current_line))
     }
+
+    pub fn generate_error(
+        content: &[&str],
+        start_pos: &CursorPos,
+        current_pos: &CursorPos,
+        message: impl Into<String>,
+    ) -> Self {
+        let (start_line, current_line) = Self::extract_lines(content, start_pos, current_pos);
+
+        UmSyntaxError {
+            start_pos: *start_pos,
+            current_pos: *current_pos,
+            start_line,
+            current_line,
+            message: message.into(),
+        }
+    }
 }
 
 impl Display for UmSyntaxError {
@@ -38,7 +56,16 @@ impl Display for UmSyntaxError {
         let start_line_number = self.start_pos.line;
         let start_symbol = self.start_pos.symbol;
 
-        f.write_fmt(format_args!("\nSyntax Error: "))?;
+        let prefix = "Syntax Error: ";
+
+        f.write_fmt(format_args!("\n{}", prefix))?;
+        for (count, line) in self.message.lines().enumerate() {
+            let prefix_len = if count > 0 { prefix.len() } else { 0 };
+
+            f.write_fmt(format_args!("{}{}\n", " ".repeat(prefix_len), line))?;
+        }
+
+        f.write_fmt(format_args!("\n"))?;
 
         f.write_fmt(format_args!("Begin reading point: \n\n"))?;
 
@@ -61,7 +88,7 @@ impl Display for UmSyntaxError {
         let curr_line_number = self.current_pos.line;
         let curr_symbol = self.current_pos.symbol;
 
-        f.write_fmt(format_args!("\nCurrent reading point: \n\n"))?;
+        f.write_fmt(format_args!("\nError occured at: \n\n"))?;
 
         let info = format!("{}:{} => ", curr_line_number + 1, curr_symbol + 1);
 
