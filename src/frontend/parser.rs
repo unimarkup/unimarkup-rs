e808d50a261fed8691befe28aa511c764eff0027
Nadir Fejzic <nadirfejzo@gmail.com>
2021-10-31 19:10:50 +0100

    Remove parse_heading function from parser

--- src/frontend/parser.rs
+++ src/frontend/parser.rs
@@ -1,5 +1,3 @@
-use crate::um_elements::heading_block::{HeadingBlock, HeadingLevel};
-
 /// CursorPos struct is used to keep track of the cursor position while parsing input.
 /// Additionally it is useful as a way to show better error messages. Using CursorPos it
 /// is possible to show exact location in the input, where parsing error is recognized.
@@ -9,33 +7,4 @@ pub struct CursorPos {
     pub line: usize,
     /// index of the symbol in the given line
     pub symbol: usize,
-}
-
-pub fn parse_heading(input: &[&str], index: &mut usize) -> HeadingBlock {
-    let mut content: String = "".into();
-
-    let mut level = HeadingLevel::Invalid;
-    let mut level_counter = 0;
-    let mut level_set = false;
-
-    while let Some(word) = input.get(*index) {
-        if word.matches('#').count() > 0 && !level_set {
-            level_counter += 1;
-        } else {
-            level_set = true;
-            level = HeadingLevel::from(level_counter);
-        }
-
-        if level_set {
-            // read content
-            content.push_str(word);
-        }
-
-        *index += 1;
-    }
-
-    HeadingBlock {
-        level,
-        content: content.trim().to_string(),
-    }
 }
