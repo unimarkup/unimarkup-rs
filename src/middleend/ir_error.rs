3ff2c77ea493915c50e83cf4048fdcf693f850f5
Nadir Fejzic <nadirfejzo@gmail.com>
2021-11-17 21:59:00 +0100

    feat: implement Display trait for IrError

--- src/middleend/ir_error.rs
+++ src/middleend/ir_error.rs
@@ -1,6 +1,38 @@
+use std::fmt;
+
 #[derive(Debug)]
 pub struct IrError {
     pub tablename: String,
     pub column: String,
     pub message: String,
+}
+
+impl fmt::Display for IrError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_fmt(format_args!("Error in communication with IR."))?;
+
+        f.write_fmt(format_args!(
+            "\nError occured in column {} of table {}.",
+            self.column, self.tablename
+        ))?;
+
+        let prefix = "Message: ";
+
+        let msg: String = self
+            .message
+            .lines()
+            .enumerate()
+            .map(|(i, line)| {
+                if i > 0 {
+                    " ".repeat(prefix.len()) + line
+                } else {
+                    String::from(line)
+                }
+            })
+            .collect();
+
+        f.write_fmt(format_args!("{}{}", prefix, msg))?;
+
+        Ok(())
+    }
 }
