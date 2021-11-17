f1a79ed4d40a5d57f7fc9971182146df9aa9d16f
Nadir Fejzic <nadirfejzo@gmail.com>
2021-11-17 22:02:48 +0100

    feat: implement UmError wrapper for error types

---
+++ src/um_error/error.rs
@@ -0,0 +1,30 @@
+use core::fmt;
+
+use crate::frontend::SyntaxError;
+use crate::middleend::IrError;
+
+enum UmError {
+    Syntax(SyntaxError),
+    Ir(IrError),
+}
+
+impl From<SyntaxError> for UmError {
+    fn from(syntax_error: SyntaxError) -> Self {
+        Self::Syntax(syntax_error)
+    }
+}
+
+impl From<IrError> for UmError {
+    fn from(ir_error: IrError) -> Self {
+        Self::Ir(ir_error)
+    }
+}
+
+impl fmt::Display for UmError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            UmError::Syntax(err) => err.fmt(f),
+            UmError::Ir(err) => err.fmt(f),
+        }
+    }
+}
