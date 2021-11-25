1586f185f9caf18b82d79c18b10a455a6659ce09
Nadir Fejzic <nadirfejzo@gmail.com>
2021-11-25 12:43:55 +0100

    feat: make UnimarkupType variants comparable

--- src/um_elements/types.rs
+++ src/um_elements/types.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use strum_macros::{Display, EnumString};
 
-#[derive(Serialize, Deserialize, EnumString, Display)]
+#[derive(Serialize, Deserialize, EnumString, Display, PartialEq)]
 #[strum(serialize_all = "snake_case")]
 pub enum UnimarkupType {
     Heading,
