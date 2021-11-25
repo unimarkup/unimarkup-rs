3f45a47e35f4c7781c220ad81afdc5454a7a3608
Manuel Hatzl <hatzlmanuel@outlook.com>
2021-11-25 22:23:01 +0100

    fix: prepare compile method

--- src/unimarkup.rs
+++ src/unimarkup.rs
@@ -1,13 +1,14 @@
-// use crate::cli;
-// use crate::frontend;
-// use crate::middleend;
-// // use crate::backend;
+use crate::cli;
+use crate::frontend;
+use crate::middleend;
+use crate::um_error::UmError;
+// use crate::backend;
 
-// pub fn compile() -> Result<_, UmError> { 
-//   let mut config = cli::get_config_from_cli();
-//   let mut connection = middleend::ir_setup::setup_ir_connection().unwrap(); // TODO: replace with ?
+pub fn compile() -> Result<bool, UmError> { 
+  let mut config = cli::get_config_from_cli();
+  let mut connection = middleend::setup_ir_connection()?;
 
-//   // frontend::run(&connection, &config)?;
-//   // backend::run(&connection, &config)?;
-//   todo!()
-// }
+  // frontend::run(&connection, &config)?;
+  // backend::run(&connection, &config)?;
+  Ok(true)
+}
