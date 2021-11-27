1068440a368c6faf0316688467693f14daaf3b51
Manuel Hatzl <hatzlmanuel@outlook.com>
2021-11-27 03:13:38 +0100

    fix: distribute all options to general and outputs

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,5 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, process};
+use log::error;
 
 use clap::{ArgEnum, Parser, crate_version};
 use strum_macros::EnumString;
@@ -16,11 +17,12 @@ pub struct Config {
     #[clap(index = 2, value_name = "OUTPUT-FILE", takes_value = true, parse(from_os_str))]
     pub out_file: Option<PathBuf>,
 
-    /// Set outputs for the Unimarkup document. The output options must be set after -O and enclosed in "" like `-O="--formats=pdf flags=custom_flag"`
+    /// Set outputs for the Unimarkup document. The output options must be set after -O and enclosed in "" like `-O="myOutputConfig --formats=pdf flags=custom_flag"`.
+    /// The first argument must be the name of the output config. This is used to compare output configurations between cli and preamble.
     /// The following output options are available:
     /// 
     /// USAGE:
-    ///    --output-formats <OUT_FORMATS>... [--] [OUTPUT-FILE]
+    ///    <OutputConfig Name> --output-formats <OUT_FORMATS>... [--] [OUTPUT-FILE]
     ///
     /// ARGS:
     ///    <OUTPUT-FILE>    The filename without filetype to be used for output filenames. If a path is
@@ -31,31 +33,11 @@ pub struct Config {
     ///            Set output formats the Unimarkup document should be rendered to. Set outputs are also
     ///            treated as flags inside the Unimarkup document [possible values: pdf, html, reveal-js, intermediate]
     ///
-    ///    -t, --theme <THEME>
-    ///            Set a Unimarkup theme file to be used for rendering [default: ]
-    ///
-    ///    -f, --flags <FLAGS>...
-    ///            Set flags that will be set for rendering
-    ///
-    ///        --relative-insert-prefix <RELATIVE_INSERT_PREFIX>
-    ///            This prefix will be set before inserts in the rendered document to inserts that use
-    ///            relative paths. Note: During rendering, the original relative path is taken [default: ]
-    ///
-    ///        --html-embed-svg
-    ///            Set if svgs should be embedded into html instead of inserted as regular images
-    ///
-    ///        --html-template <HTML_TEMPLATE>
-    ///            Set a template html file with `{{ head }}` set inside the `head` element and `{{ body
-    ///            }}` set inside the body element. Styling, fonts and scripts will be inserted at `{{ head
-    ///            }}` and the rendered Unimarkup content is placed inside `{{ body }}`. Optionally, `{{
-    ///            toc }}` can be set to get the table of contents (Note: This will not remove a rendered
-    ///            table of contents inside the rendered Unimarkup content if present) [default: ]
-    ///
-    ///        --html-mathmode <HTML_MATHMODE>
-    ///            Set the mathmode of MathJax to be used for rendered html documents [default: svg] [possible values: svg, embed, cdn]
+    /// Same additional options as for the global configuration
+    /// 
     // TODO: #[doc = stringify!(OutputConfig::parse_from(vec![UNIMARKUP_NAME, "--help"]))]
     #[clap(display_order = 1, short = 'O', long = "output", parse(try_from_str = parse_output_config), multiple_occurrences(true), takes_value = true)]
-    pub outputs: Option<Vec<OutputConfig>>,
+    pub outputs: Option<Vec<(String, OutputConfig)>>,
 
     /// Set paths that are searched for relative file and image inserts.
     #[clap(display_order = 2, long = "insert-paths", takes_value = true, use_delimiter = true, parse(from_os_str))]
@@ -109,23 +91,46 @@ pub struct Config {
     pub rebuild: Option<bool>,
 
     /// Set if preamble of given Unimarkup file is replaced with the given arguments.
-    /// If not set, given arguments overwrite the corresponding preamble settings, but other settings
-    /// are still used.
+    /// If not set, given arguments overwrite the corresponding preamble settings, but other settings are still used.
+    /// 
+    /// Note: Only the preamble part of an output config with the same name is replaced if this flag is only set inside an output config   
     #[clap(display_order = 20, long = "replace-preamble", requires = "outputs")]
     pub replace_preamble: Option<bool>,
+
+    /// This prefix will be set before inserts in the rendered document to inserts that use relative paths.
+    /// Note: During rendering, the original relative path is taken.
+    #[clap(display_order = 20, long = "relative-insert-prefix", alias = "insert-prefix", takes_value = true, parse(from_os_str))]
+    pub relative_insert_prefix: Option<PathBuf>,
+
+    /// Set a template html file with `{{ head }}` set inside the `head` element and `{{ body }}` set inside the body element.
+    /// Styling, fonts and scripts will be inserted at `{{ head }}` and the rendered Unimarkup content is placed inside `{{ body }}`.
+    /// Optionally, `{{ toc }}` can be set to get the table of contents (Note: This will not remove a rendered table of contents inside the rendered Unimarkup content if present).
+    #[clap(display_order = 40, long = "html-template", takes_value = true, parse(from_os_str))]
+    pub html_template: Option<PathBuf>, 
+
+    /// Set the mathmode of MathJax to be used for rendered html documents.
+    #[clap(display_order = 41, long = "html-mathmode", takes_value = true, arg_enum)]
+    pub html_mathmode: Option<HtmlMathmode>,
+
+    /// Set if svgs should be embedded into html instead of inserted as regular images.
+    #[clap(display_order = 40, long = "html-embed-svg")]
+    pub html_embed_svg: Option<bool>,
 }
 
 /// Parse a single output config 
-fn parse_output_config(s: &str) -> Result<OutputConfig, clap::Error>
+fn parse_output_config(s: &str) -> Result<(String, OutputConfig), clap::Error>
 {
-    let args: Vec<&str> = s.trim().split_whitespace().collect();
-    let mut params = vec![UNIMARKUP_NAME];
-    params.extend(args);
+    let args : Vec<String> = shlex::Shlex::new(s).collect(); // s.trim().split_whitespace().collect();
+    //let mut params = vec![UNIMARKUP_NAME];
+    //params.extend(args);
+    if args.is_empty() {
+        error!("No output config name was given! Every output config must start with a unique name!");
+        process::exit(1)
+    }
 
-    println!("{:?}", &params);
-
-    let out_config = OutputConfig::try_parse_from(params)?;
-    Ok(out_config)
+    let cfg_name = args[0].clone();
+    let out_config = OutputConfig::try_parse_from(args)?;
+    Ok((cfg_name, out_config))
 }
 
 #[derive(Debug, PartialEq, Clone, Parser)]
@@ -135,8 +140,17 @@ pub struct OutputConfig {
     pub out_file: Option<PathBuf>,
 
     /// Set output formats the Unimarkup document should be rendered to. Set outputs are also treated as flags inside the Unimarkup document.
-    #[clap(display_order = 1, long = "output-formats", alias = "formats", takes_value = true, use_delimiter = true, required = true, arg_enum)]
+    #[clap(name = "output-formats",display_order = 1, long = "output-formats", alias = "formats", takes_value = true, use_delimiter = true, required = true, arg_enum)]
     pub out_formats: Vec<OutputFormat>,
+
+    /// Set paths that are searched for relative file and image inserts.
+    #[clap(display_order = 2, long = "insert-paths", takes_value = true, use_delimiter = true, parse(from_os_str))]
+    pub insert_paths: Option<Vec<PathBuf>>,
+
+    /// Set the path to a directory that is used for default preamble and theme settings.
+    /// The intermediate form of rendered documents will also be stored at this path.
+    #[clap(display_order = 3, long = "dot-unimarkup", alias = "config", takes_value = true, env = "UNIMARKUP_CONFIG", parse(from_os_str))]
+    pub dot_unimarkup: Option<PathBuf>,
 
     /// Set a Unimarkup theme file to be used for rendering.
     #[clap(display_order = 5, short = 't', long = "theme", takes_value = true, parse(from_os_str))]
@@ -145,6 +159,47 @@ pub struct OutputConfig {
     /// Set flags that will be set for rendering.
     #[clap(display_order = 6, short = 'f', long = "flags", takes_value = true, use_delimiter = true)]
     pub flags: Option<Vec<String>>,
+
+    /// Explicitly set Unimarkup block elements that can be used inside the given Unimarkup document.
+    /// If this option is set, all Unimarkup elements that are not given are disabled.
+    #[clap(display_order = 7, long = "enable-elements", takes_value = true, use_delimiter = true, arg_enum)]
+    pub enable_elements: Option<Vec<UmBlockElements>>,
+
+    /// Explicitly set Unimarkup block elements that can NOT be used inside the given Unimarkup document.
+    /// If this option is set, all Unimarkup elements that are not given are enabled.
+    #[clap(display_order = 8, long = "disable-elements", takes_value = true, use_delimiter = true, arg_enum)]
+    pub disable_elements: Option<Vec<UmBlockElements>>,
+
+    /// Set citation style sheet that is used to process referenced literature
+    #[clap(display_order = 30, long = "citation-style", alias = "csl", takes_value = true, requires = "references", parse(from_os_str))]
+    pub citation_style: Option<PathBuf>,
+
+    /// Set one or more reference files in bibtex or JSON format to use them with literature referencing.
+    #[clap(display_order = 31, long = "references", alias = "refs", takes_value = true, use_delimiter = true, requires = "citation-style", parse(from_os_str))]
+    pub references: Option<Vec<PathBuf>>,
+
+    /// Set ttf or woff fonts to be able to use them for rendering
+    #[clap(display_order = 10, long = "fonts", alias = "ttf", alias = "woff", takes_value = true, use_delimiter = true, parse(from_os_str))]
+    pub fonts: Option<Vec<PathBuf>>,
+
+    /// Overwrites files with set `filename` if already existing.
+    #[clap(display_order = 1, short = 'w', long = "overwrite-existing")]
+    pub overwrite_existing: Option<bool>,
+
+    /// Deletes all previously rendered documents stored inside the UNIMARKUP_CONFIG path.
+    #[clap(display_order = 2, short = 'c', long = "clean")]
+    pub clean: Option<bool>,
+
+    /// Ignores all previously rendered documents stored inside the UNIMARKUP_CONFIG path and renders the given Unimarkup file.
+    #[clap(display_order = 3, short = 'r', long = "rebuild")]
+    pub rebuild: Option<bool>,
+
+    /// Set if preamble of given Unimarkup file is replaced with the given arguments.
+    /// If not set, given arguments overwrite the corresponding preamble settings, but other settings are still used.
+    /// 
+    /// Note: Only the preamble part of an output config with the same name is replaced if this flag is only set inside an output config   
+    #[clap(display_order = 20, long = "replace-preamble", requires = "output-formats")]
+    pub replace_preamble: Option<bool>,
 
     /// This prefix will be set before inserts in the rendered document to inserts that use relative paths.
     /// Note: During rendering, the original relative path is taken.
--- tests/cli.rs
+++ tests/cli.rs
@@ -4,13 +4,17 @@ use clap::Parser;
 use shlex::Shlex;
 use unimarkup_rs::{config::{Config, OutputFormat}, um_error::UmError};
 
+fn get_args(options: &str, um_file: &str) -> Vec<String> {
+  let arg_line = format!("unimarkup {} {}", options, um_file);
+  Shlex::new(&arg_line).collect()
+}
 
 #[test]
 fn only_required_arguments_to_struct() -> Result<(), UmError> {
   let um_filename = "file.um";
-  let cfg: Config = Config::parse_from(vec!["unimarkup", um_filename]);
+  let cfg: Config = Config::parse_from(get_args("", um_filename));
 
-  assert_eq!(cfg.um_file.to_str().unwrap(), um_filename, "Unimarkup filename set correctly");
+  assert_eq!(cfg.um_file.to_str().unwrap(), um_filename, "Unimarkup filename not set correctly");
 
   Ok(())
 }
@@ -18,14 +22,16 @@ fn only_required_arguments_to_struct() -> Result<(), UmError> {
 #[test]
 fn single_output_config() -> Result<(), UmError> {
   let um_filename = "file.um";
-  let output = "-O --output-formats=html,pdf";
+  let out_name = "out";
+  let output = format!("-O=\"{} --output-formats=html,pdf\"", out_name);
 
-  let cfg: Config = Config::parse_from(vec!["unimarkup", output, um_filename]);
+  let cfg: Config = Config::parse_from(get_args(&output, um_filename));
   let cfg_outputs = cfg.outputs.unwrap();
 
-  assert_eq!(cfg_outputs[0].out_formats[0], OutputFormat::Html, "Unimarkup html output format set correctly");
-  assert_eq!(cfg_outputs[0].out_formats[1], OutputFormat::Pdf, "Unimarkup html output format set correctly");
-  assert_eq!(cfg.um_file.to_str().unwrap(), um_filename, "Unimarkup filename set correctly");
+  assert_eq!(cfg_outputs[0].1.out_formats[0], OutputFormat::Html, "Unimarkup html output format not set correctly");
+  assert_eq!(cfg_outputs[0].1.out_formats[1], OutputFormat::Pdf, "Unimarkup html output format not set correctly");
+  assert_eq!(cfg.um_file.to_str().unwrap(), um_filename, "Unimarkup filename not set correctly");
+  assert_eq!(cfg_outputs[0].0, out_name, "Output config name not set correctly");
 
   Ok(())
 }
@@ -33,26 +39,45 @@ fn single_output_config() -> Result<(), UmError> {
 #[test]
 fn multiple_output_configs() -> Result<(), UmError> {
   let um_filename = "file.um";
-  let output = "-O=\"--output-formats=html,pdf\"";
+  let out_1_name = "out_1";
+  let output_1 = format!("-O=\"{} --output-formats=html,pdf\"", out_1_name);
   let theme = "test_theme.um";
-  let output_2 = format!("-O=\"--output-formats=intermediate,revealjs --theme={}\"", theme);
+  let out_2_name = "out_2";
+  let output_2 = format!("-O=\"{} --output-formats=intermediate,revealjs --theme={}\"", out_2_name, theme);
 
-  let arg_line = format!("unimarkup {} {} {}", output, output_2, um_filename);
-  let args = Shlex::new(&arg_line);
+  let options = format!("{} {}", output_1, output_2);
+  let args = get_args(&options, um_filename);
 
   let cfg: Config = Config::parse_from(args);
   let cfg_outputs = cfg.outputs.unwrap();
 
-  assert_eq!(cfg_outputs[0].out_formats[0], OutputFormat::Html, "Unimarkup html output format set correctly");
-  assert_eq!(cfg_outputs[0].out_formats[1], OutputFormat::Pdf, "Unimarkup html output format set correctly");
-  assert!(cfg_outputs[0].out_formats.len() == 2, "Too many Unimarkup output formats set");
+  assert_eq!(cfg_outputs[0].1.out_formats[0], OutputFormat::Html, "Unimarkup html output format not set correctly");
+  assert_eq!(cfg_outputs[0].1.out_formats[1], OutputFormat::Pdf, "Unimarkup html output format not set correctly");
+  assert!(cfg_outputs[0].1.out_formats.len() == 2, "Too many Unimarkup output formats set");
+  assert_eq!(cfg_outputs[0].0, out_1_name, "Output_1 config name not set correctly");
 
-  assert_eq!(cfg_outputs[1].out_formats[0], OutputFormat::Intermediate, "Unimarkup intermediate output format set correctly");
-  assert_eq!(cfg_outputs[1].out_formats[1], OutputFormat::RevealJs, "Unimarkup revealJs output format set correctly");
-  assert_eq!(cfg_outputs[1].theme, Some(PathBuf::from(theme)), "Unimarkup theme for second output config set correctly");
-  assert!(cfg_outputs[1].out_formats.len() == 2, "Too many Unimarkup output formats set");
+  assert_eq!(cfg_outputs[1].1.out_formats[0], OutputFormat::Intermediate, "Unimarkup intermediate output format not set correctly");
+  assert_eq!(cfg_outputs[1].1.out_formats[1], OutputFormat::RevealJs, "Unimarkup revealJs output format not set correctly");
+  assert_eq!(cfg_outputs[1].1.theme, Some(PathBuf::from(theme)), "Unimarkup theme for second output config not set correctly");
+  assert!(cfg_outputs[1].1.out_formats.len() == 2, "Too many Unimarkup output formats set");
+  assert_eq!(cfg_outputs[1].0, out_2_name, "Output_2 config name not set correctly");
 
-  assert_eq!(cfg.um_file.to_str().unwrap(), um_filename, "Unimarkup filename set correctly");
+  assert_eq!(cfg.um_file.to_str().unwrap(), um_filename, "Unimarkup filename not set correctly");
+
+  Ok(())
+}
+
+#[test]
+fn bad_theme_path() -> Result<(), UmError> {
+  let um_filename = "file.um";
+  let theme = "not_existing_theme.um";
+
+  let options = format!("--theme={}", theme);
+  let args = get_args(&options, um_filename);
+
+  let cfg: Config = Config::parse_from(args);
+
+  assert!(!cfg.theme.unwrap().exists(), "Theme file should not exist");
 
   Ok(())
 }
