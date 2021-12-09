db5c114a70587356abd354827fcdd91d4b43cdab
Nadir Fejzic <nadirfejzo@gmail.com>
2021-12-09 20:49:09 +0100

    style: format code using with cargo fmt

--- src/unimarkup.rs
+++ src/unimarkup.rs
@@ -4,10 +4,10 @@ use crate::middleend;
 use crate::um_error::UmError;
 // use crate::backend;
 
-pub fn compile(config: Config) -> Result<(), UmError> { 
-  let mut connection = middleend::setup_ir_connection()?;
+pub fn compile(config: Config) -> Result<(), UmError> {
+    let mut connection = middleend::setup_ir_connection()?;
 
-  // frontend::run(&connection, &mut config)?;
-  // backend::run(&connection, &config)?;
-  Ok(())
+    // frontend::run(&connection, &mut config)?;
+    // backend::run(&connection, &config)?;
+    Ok(())
 }
