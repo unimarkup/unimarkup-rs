defb7d64b7c0311e1ea63b42197820d7afd7887d
Nadir Fejzic <nadirfejzo@gmail.com>
2021-12-23 01:43:03 +0100

    docs: document the UmError

--- src/um_error/error.rs
+++ src/um_error/error.rs
@@ -5,11 +5,34 @@ use crate::backend::BackendError;
 use crate::frontend::SyntaxError;
 use crate::middleend::IrError;
 
+/// Unimarkup error which wraps [`SyntaxError`], [`IrError`], [`BackendError`] and
+/// any other type which implements the [`Error`] trait.
+///
+/// [`SyntaxError`]: crate::frontend::SyntaxError
+/// [`IrError`]: crate::middleend::IrError
+/// [`BackendError`]: crate::backend::BackendError
 pub enum UmError {
+    /// Represents a syntax error in input unimarkup document.
     Syntax(SyntaxError),
+
+    /// Represents an error in communication with the IR.
     Ir(IrError),
+
+    /// Represents an error occured in backend when, for example,
+    /// (re)constructing [`UnimarkupBlocks`] from IR.
+    ///
+    /// [`UnimarkupBlocks`]: crate::frontend::UnimarkupBlocks
     Backend(BackendError),
-    General { msg: String, error: Box<dyn Error> },
+
+    /// Wrapper for any other type implementing the [`Error`] trait, for
+    /// cases where neither of the previous three variants apply.
+    General {
+        /// Custom error message for better explanation
+        msg: String,
+
+        /// The actual error which occured.
+        error: Box<dyn Error>,
+    },
 }
 
 impl From<SyntaxError> for UmError {
--- src/um_error/mod.rs
+++ src/um_error/mod.rs
@@ -1,3 +1,18 @@
+//! Unimarkup Error used in [`frontend`], [`backend`] and [`middleend`] modules.
+//!
+//! It's used to describe various errors which can occur during the compilation
+//! of unimarkup document, e.g. [`SyntaxError`] when parsing the document, [`IrError`]
+//! when communication with IR fails, or [`BackendError`] when constructing
+//! [`UnimarkupBlocks`] in the [`backend`].
+//!
+//! [`frontend`]: crate::frontend
+//! [`middleend`]: crate::middleend
+//! [`backend`]: crate::backend
+//! [`SyntaxError`]: crate::frontend::SyntaxError
+//! [`IrError`]: crate::middleend::IrError
+//! [`BackendError`]: crate::backend::BackendError
+//! [`UnimarkupBlocks`]: crate::frontend::UnimarkupBlocks
+
 mod error;
 
 pub use error::UmError;
