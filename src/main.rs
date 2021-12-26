fa67333c84be78c7b664daf6c1afee4ced5c404f
Nadir Fejzic <nadirfejzo@gmail.com>
2021-12-26 17:09:43 +0100

    fix: use env_logger instead of simple_logger

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
+use std::io::Write;
+
 use clap::Parser;
-use simple_logger::SimpleLogger;
+use log::LevelFilter;
 use unimarkup_rs::{config::Config, unimarkup};
 
 fn main() {
-    SimpleLogger::new().init().unwrap();
+    env_logger::builder()
+        .format_level(true)
+        .filter_level(LevelFilter::Info)
+        .format_timestamp(None)
+        .format_target(false)
+        .format_module_path(false)
+        .format_suffix("")
+        // .format(|buf, record| {
+        //     // buf.write_fmt(format_args!("{}: {}\n", record.level(), record.args()))
+        //     writeln!(buf, "{}", record.level().)
+        // })
+        .init();
+
     let config = Config::parse();
 
     match unimarkup::compile(config) {
