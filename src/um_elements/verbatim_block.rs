f274017535f1a826ccdcad629064a2a9578c0901
Nadir Fejzic <nadirfejzo@gmail.com>
2022-01-06 22:26:53 +0100

    feat: implement AsIrLines for VerbatimBlock

--- src/um_elements/verbatim_block.rs
+++ src/um_elements/verbatim_block.rs
@@ -1,3 +1,8 @@
+use crate::{
+    middleend::{AsIrLines, ContentIrLine},
+    um_elements::types::UnimarkupType,
+};
+
 /// Structure of a Unimarkup verbatim block element.
 pub struct VerbatimBlock {
     /// Unique identifier for a verbatim block.
@@ -12,4 +17,20 @@ pub struct VerbatimBlock {
     /// Line number, where the verbatim block occurs in
     /// the Unimarkup document.
     pub line_nr: usize,
+}
+
+impl AsIrLines for VerbatimBlock {
+    fn as_ir_lines(&self) -> Vec<ContentIrLine> {
+        let line = ContentIrLine::new(
+            &self.id,
+            self.line_nr,
+            UnimarkupType::VerbatimBlock.to_string(),
+            &self.content,
+            "",
+            &self.attributes,
+            "",
+        );
+
+        vec![line]
+    }
 }
