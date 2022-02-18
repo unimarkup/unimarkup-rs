243da563987ac70fdd7247232070f1f89f789cac
Julius Šula <julius.sula@gmail.com>
2022-02-18 11:29:16 +0100

    chore: inline_test and metadata from merge

--- core/tests/elements/metadata.rs
+++ core/tests/elements/metadata.rs
@@ -10,11 +10,7 @@ use unimarkup_core::{
 };
 
 #[test]
-<<<<<<< HEAD
 fn test_ir_root_metadata_in_ir() {
-=======
-fn root_metadata_in_ir() {
->>>>>>> main
     let testfile = "tests/test_files/small_testfile.um";
 
     let mut connection = ir_test_setup::setup_test_ir();
@@ -22,13 +18,9 @@ fn root_metadata_in_ir() {
 
     let input = std::fs::read_to_string(&cfg.um_file).unwrap();
 
-<<<<<<< HEAD
     let result = frontend::run(&input, &mut connection, &mut cfg);
 
     assert!(result.is_ok(), "Cause: {:?}", result.unwrap_err());
-=======
-    frontend::run(&input, &mut connection, &mut cfg).unwrap();
->>>>>>> main
 
     let expected_metadata = Metadata {
         file: Path::new(testfile).to_path_buf(),
@@ -39,7 +31,6 @@ fn root_metadata_in_ir() {
 
     let ir_metadata: MetadataIrLine = expected_metadata.into();
 
-<<<<<<< HEAD
     let transaction = connection.transaction();
 
     assert!(
@@ -54,14 +45,4 @@ fn root_metadata_in_ir() {
 
     let metadata_exists = middleend::entry_already_exists(&ir_metadata, &transaction.unwrap());
     assert!(metadata_exists, "Metadata does not exist");
-=======
-    match connection.transaction() {
-        Ok(transaction) => {
-            let metadata_exists = middleend::entry_already_exists(&ir_metadata, &transaction);
-
-            assert!(metadata_exists);
-        }
-        Err(_) => panic!("Failed creating database connection"),
-    };
->>>>>>> main
 }
