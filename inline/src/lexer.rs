eaf3a7435c1023c11cd367a9eb9e32bb1f6b7e7d
Nadir Fejzic <nadirfejzo@gmail.com>
2022-03-28 20:47:22 +0200

    test: add helper macro for token tests

--- inline/src/lexer.rs
+++ inline/src/lexer.rs
@@ -412,6 +412,21 @@ impl IsKeyword for &str {
 mod tests {
     use super::*;
 
+    macro_rules! assert_token {
+        ($token:ident with $kind:expr, $spacing:expr, $span:expr) => {
+            assert_eq!($token.kind(), $kind);
+            assert_eq!($token.spacing(), $spacing);
+            assert_eq!($token.span(), Span::from($span));
+            true
+        };
+
+        ($token:ident with $kind:expr, $spacing:expr, $span:expr, $content:expr) => {
+            assert_token!($token with $kind, $spacing, $span);
+            assert_eq!($token.as_str(), $content);
+            true
+        }
+    }
+
     #[test]
     fn lines() {
         let input = r#"first line
