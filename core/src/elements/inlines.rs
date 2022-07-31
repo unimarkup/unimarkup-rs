96055ed3b4558eaa34588499898323eb94e1e37f
Nadir Fejzic <nadirfejzo@gmail.com>
2022-07-31 21:14:29 +0200

    feat: impl `Render` for `unimarkup_inline::Inline`

---
+++ core/src/elements/inlines.rs
@@ -0,0 +1,106 @@
+//! Implementation of Render trait for Unimarkup inlines
+
+use unimarkup_inline::{Inline, InlineContent, TokenDelimiters, TokenKind};
+
+use crate::backend::{error::BackendError, Render};
+
+impl Render for TokenDelimiters {
+    fn render_html(&self) -> Result<String, BackendError> {
+        let open_tag = match self.open() {
+            TokenKind::Bold => "<strong>",
+            TokenKind::Italic => "<em>",
+            TokenKind::ItalicBold => "<em><strong>",
+            TokenKind::Underline => "<span style='text-decoration: underline;'>",
+            TokenKind::Subscript => "<sub>",
+            TokenKind::UnderlineSubscript => "<sub style='text-decoration: underline'>",
+            TokenKind::Superscript => "<sup>",
+            TokenKind::Overline => "<span style='text-decoration: overline;'>",
+            TokenKind::Strikethrough => "<span style='text-decoration: line-through;'>",
+            TokenKind::Highlight => "<span style='background-color: #ffaaaa;'>",
+            TokenKind::Verbatim => "<pre><code>",
+            TokenKind::Quote => "<span class='quote'>",
+            TokenKind::Math => "<span class='math'>",
+            TokenKind::OpenParens => "(",
+            TokenKind::CloseParens => ")",
+            TokenKind::OpenBracket => "[",
+            TokenKind::CloseBracket => "]",
+            TokenKind::OpenBrace => "{",
+            TokenKind::CloseBrace => "}",
+            TokenKind::Substitution => "",
+            TokenKind::Newline => "<br />",
+            TokenKind::EndOfLine => "<br />",
+            TokenKind::Whitespace => "&nbsp",
+            TokenKind::Plain => "",
+        };
+
+        let mut tags = String::from(open_tag);
+
+        if let Some(close_tag) = self.close() {
+            let close_tag = match close_tag {
+                TokenKind::Bold => "</strong>",
+                TokenKind::Italic => "</em>",
+                TokenKind::ItalicBold => "</strong></em>",
+                TokenKind::Underline => "</span>",
+                TokenKind::Subscript => "</sub>",
+                TokenKind::UnderlineSubscript => "</sub>",
+                TokenKind::Superscript => "</sup>",
+                TokenKind::Overline => "</span>",
+                TokenKind::Strikethrough => "</span>",
+                TokenKind::Highlight => "</span>",
+                TokenKind::Verbatim => "</code></pre>",
+                TokenKind::Quote => "</span>",
+                TokenKind::Math => "</span>",
+                TokenKind::OpenParens => "(",
+                TokenKind::CloseParens => ")",
+                TokenKind::OpenBracket => "[",
+                TokenKind::CloseBracket => "]",
+                TokenKind::OpenBrace => "{",
+                TokenKind::CloseBrace => "}",
+                TokenKind::Substitution => "",
+                TokenKind::Newline => "<br />",
+                TokenKind::EndOfLine => "<br />",
+                TokenKind::Whitespace => "&nbsp",
+                TokenKind::Plain => "",
+            };
+
+            tags.push('|');
+            tags.push_str(close_tag);
+        }
+
+        Ok(tags)
+    }
+}
+
+impl Render for Inline {
+    fn render_html(&self) -> Result<String, BackendError> {
+        let mut res = String::new();
+        let tags = self.delimiters().render_html()?;
+        let mut tags = tags.split('|');
+
+        if let Some(open_tag) = tags.next() {
+            res.push_str(open_tag);
+        }
+
+        let content = match self.as_ref() {
+            InlineContent::Plain(plain_content) => String::from(plain_content.as_str()),
+            InlineContent::Nested(nested_inlines) => {
+                let mut content = String::new();
+
+                for inline in nested_inlines.iter() {
+                    let html = inline.render_html()?;
+                    content.push_str(&html);
+                }
+
+                content
+            }
+        };
+
+        res.push_str(&content);
+
+        if let Some(close_tag) = tags.next() {
+            res.push_str(close_tag);
+        }
+
+        Ok(res)
+    }
+}
