43fd0059c44d638317dd61f07b6e8d2f3061bccf
Nadir Fejzic <nadirfejzo@gmail.com>
2023-04-13 21:47:36 +0200

    feat: add type alias for parser functions

--- core/src/parser/mod.rs
+++ core/src/parser/mod.rs
@@ -1,1 +1,8 @@
 mod symbol;
+
+use symbol::Symbol;
+
+use crate::elements::Blocks;
+
+/// Parser as function that can parse Unimarkup content
+pub type ParserFn = dyn for<'i> Fn(&'i [Symbol<'i>]) -> Option<(Blocks, &'i [Symbol<'i>])>;
