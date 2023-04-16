e39588a53b16c62477d268fc06195b9ccba82711
Nadir Fejzic <nadirfejzo@gmail.com>
2023-04-16 18:56:28 +0200

    docs: add todo comment about module privacy

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,6 +1,7 @@
 #![warn(missing_docs)]
 //! The unimarkup-rs crate is the official implementation of the [Unimarkup specification](https://github.com/Unimarkup/Specification/).
 
+// TODO: set to private modules that don't have to be public
 pub mod config;
 pub mod document;
 pub mod elements;
