15b6ca738634d802069810b9924b612ddf0d759f
Nadir Fejzic <nadirfejzo@gmail.com>
2023-04-19 03:23:16 +0200

    refactor: remove pest parsing of paragraph

--- core/src/elements/atomic/paragraph.rs
+++ core/src/elements/atomic/paragraph.rs
@@ -1,25 +1,19 @@
-use std::{collections::HashMap, fmt::Debug};
+use std::fmt::Debug;
 
-use logid::{
-    capturing::{LogIdTracing, MappedLogId},
-    log_id::LogId,
-};
-use pest::iterators::Pairs;
-use pest::Span;
+use logid::capturing::MappedLogId;
 use unimarkup_inline::{Inline, ParseUnimarkupInlines};
 use unimarkup_render::{html::Html, render::Render};
 
 use crate::{
     elements::{blocks::Block, types},
-    frontend::parser::{self, custom_pest_error, Rule, UmParse},
-    log_id::CORE_LOG_ID_MAP,
+    frontend::parser,
     parser::{
         symbol::{Symbol, SymbolKind},
         TokenizeOutput,
     },
 };
 use crate::{
-    elements::{inlines, log_id::GeneralErrLogId, Blocks},
+    elements::{inlines, Blocks},
     parser::ElementParser,
 };
 
@@ -60,67 +54,6 @@ impl From<&[Symbol<'_>]> for Paragraph {
             attributes: None,
             line_nr,
         }
-    }
-}
-
-impl UmParse for Paragraph {
-    fn parse(pairs: &mut Pairs<Rule>, span: Span) -> Result<Blocks, MappedLogId>
-    where
-        Self: Sized,
-    {
-        let (line_nr, _column_nr) = span.start_pos().line_col();
-
-        let mut paragraph_rules = pairs
-            .next()
-            .expect("paragraph must be there at this point")
-            .into_inner();
-
-        let content = paragraph_rules
-            .next()
-            .expect("Invalid paragraph: content expected")
-            .as_str()
-            .parse_unimarkup_inlines()
-            .collect();
-
-        let attributes = if let Some(attributes) = paragraph_rules.next() {
-            let attr: HashMap<&str, &str> =
-                serde_json::from_str(attributes.as_str()).map_err(|err| {
-                    (GeneralErrLogId::InvalidAttribute as LogId)
-                        .set_event_with(
-                            &CORE_LOG_ID_MAP,
-                            &custom_pest_error(
-                                "Paragraph attributes are not valid JSON.",
-                                attributes.as_span(),
-                            ),
-                            file!(),
-                            line!(),
-                        )
-                        .add_info(&format!("Cause: {}", err))
-                })?;
-
-            Some(attr)
-        } else {
-            None
-        };
-
-        let id = match attributes {
-            Some(ref attrs) if attrs.get("id").is_some() => attrs.get("id").unwrap().to_string(),
-            _ => parser::generate_id(&format!(
-                "paragraph{delim}{}",
-                line_nr,
-                delim = types::ELEMENT_TYPE_DELIMITER
-            ))
-            .unwrap(),
-        };
-
-        let paragraph_block = Paragraph {
-            id,
-            content,
-            attributes: serde_json::to_string(&attributes.unwrap_or_default()).ok(),
-            line_nr,
-        };
-
-        Ok(vec![paragraph_block.into()])
     }
 }
 
