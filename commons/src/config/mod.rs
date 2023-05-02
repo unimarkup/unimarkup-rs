32454d744e83b85b591820fde76c66fe81964a72
Manuel Hatzl <hatzlmanuel@outlook.com>
2023-05-02 23:14:24 +0200

    feat: add basic clap macros to config structs

--- commons/src/config/mod.rs
+++ commons/src/config/mod.rs
@@ -1,75 +1,138 @@
-use std::{path::PathBuf, collections::{HashMap, HashSet}};
+use std::{
+    collections::{HashMap, HashSet},
+    path::PathBuf,
+    str::FromStr,
+};
 
+use clap::{Args, Parser, ValueEnum};
 
-
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
 pub struct Config {
-  pub preamble: Preamble,
-  pub merging: MergingConfig,
-  pub input: PathBuf,
+    #[command(flatten)]
+    pub preamble: Preamble,
+    #[command(flatten)]
+    pub merging: MergingConfig,
+    pub input: PathBuf,
 }
 
+#[derive(Args, Clone)]
 pub struct MergingConfig {
-  pub ignore_preamble: bool,
+    #[arg(long)]
+    pub ignore_preamble: bool,
 }
 
+#[derive(Args, Clone)]
 pub struct Preamble {
-  pub output: Output,
-  pub metadata: Metadata,
-  pub cite: Citedata,
-  pub render: RenderConfig,
-  pub i18n: I18n,
+    #[command(flatten)]
+    pub output: Output,
+    #[command(flatten)]
+    pub metadata: Metadata,
+    #[command(flatten)]
+    pub cite: Citedata,
+    #[command(flatten)]
+    pub render: RenderConfig,
+    #[command(flatten)]
+    pub i18n: I18n,
 }
 
+#[derive(Args, Clone)]
 pub struct I18n {
-  pub lang: Option<String>,
-  pub langs: HashSet<String>,
+    #[arg(default_value_t = String::from("en-US"))]
+    pub lang: String,
+    #[arg(long, value_parser = parse_to_hashset::<String>)]
+    pub langs: HashSet<String>,
 }
 
-
+#[derive(Args, Clone)]
 pub struct RenderConfig {
-  /// K = element/attribute name, V = `true` to ignore
-  pub ignore: HashMap<String, bool>, 
-  pub parameter: HashMap<String, String>,
-  pub keep_comments: bool,
-  pub non_strict: bool,
+    #[arg(long, value_parser = parse_to_hashset::<String>)]
+    pub ignore: HashSet<String>,
+    #[arg(long, value_parser = parse_parameter)]
+    pub parameter: HashMap<String, String>,
+    pub keep_comments: bool,
+    pub non_strict: bool,
 }
-
 
 // TODO: Instead of PathBufs, file contents should be parsed on deserialization.
 // This makes it easier to access the parsed data without creating another config struct.
 // It also makes compiling faster for bad inputs, since it would break before parsing starts.
+#[derive(Args, Clone)]
 pub struct Citedata {
-  pub style: Option<PathBuf>,
-  pub references: HashSet<PathBuf>,
+    pub style: Option<PathBuf>,
+    #[arg(long, value_parser = parse_to_hashset::<PathBuf>)]
+    pub references: HashSet<PathBuf>,
 }
 
+#[derive(Args, Clone)]
 pub struct Metadata {
-  pub title: Option<String>,
-  pub authors: HashSet<String>,
-  pub description: Option<String>,
-  pub base: Option<PathBuf>,
-  pub fonts: HashSet<PathBuf>,
+    pub title: Option<String>,
+    #[arg(long, value_parser = parse_to_hashset::<String>)]
+    pub authors: HashSet<String>,
+    pub description: Option<String>,
+    pub base: Option<PathBuf>,
+    #[arg(long, value_parser = parse_to_hashset::<PathBuf>)]
+    pub fonts: HashSet<PathBuf>,
 }
 
-
+#[derive(Args, Clone)]
 pub struct Output {
-  pub file: Option<PathBuf>,
-  pub formats: HashSet<OutputFormat>,
-  pub format_specific: OutputFormatSpecific,
-  /// `true` overwrites existing output files
-  pub overwrite: bool,
+    pub file: Option<PathBuf>,
+    #[arg(long, value_parser = parse_to_hashset::<OutputFormat>)]
+    pub formats: HashSet<OutputFormat>,
+    #[command(flatten)]
+    pub format_specific: OutputFormatSpecific,
+    /// `true` overwrites existing output files
+    pub overwrite: bool,
 }
 
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Hash)]
 pub enum OutputFormat {
-  Html,
+    Html,
 }
 
+impl FromStr for OutputFormat {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "html" => Ok(OutputFormat::Html),
+            o => Err(format!("Bad output format: {}", o)),
+        }
+    }
+}
+
+#[derive(Args, Clone)]
 pub struct OutputFormatSpecific {
-  pub html: HtmlSpecific,
+    #[command(flatten)]
+    pub html: HtmlSpecific,
 }
 
+#[derive(Args, Clone)]
 pub struct HtmlSpecific {
-  pub favicons: HashSet<PathBuf>,
-  pub keywords: HashSet<String>,
+    #[arg(long, value_parser = parse_to_hashset::<PathBuf>)]
+    pub favicons: HashSet<PathBuf>,
+    #[arg(long, value_parser = parse_to_hashset::<String>)]
+    pub keywords: HashSet<String>,
 }
 
+pub fn parse_to_hashset<T>(s: &str) -> Result<HashSet<T>, clap::Error>
+where
+    T: std::str::FromStr + std::cmp::Eq + std::hash::Hash,
+    <T as std::str::FromStr>::Err: std::fmt::Debug,
+{
+    let try_entries: Result<Vec<T>, _> = s.split(',').map(|e| T::from_str(e.trim())).collect();
+    let entries = try_entries.map_err(|err| {
+        clap::Error::raw(
+            clap::error::ErrorKind::InvalidValue,
+            format!("HashSet conversion failed with: {:?}", err),
+        )
+    });
+    Ok(HashSet::from_iter(entries?.into_iter()))
+}
+
+pub fn parse_parameter(_s: &str) -> Result<HashMap<String, String>, clap::Error> {
+    //TODO: Implement once parameter parser is implemented
+
+    Ok(HashMap::default())
+}
