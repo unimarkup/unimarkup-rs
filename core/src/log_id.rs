28432de4e98af56a223534f2091869569e8f21e1
Manuel Hatzl <hatzlmanuel@outlook.com>
2023-05-07 00:34:30 +0200

    refactor: remove config log-ids from core

--- core/src/log_id.rs
+++ core/src/log_id.rs
@@ -2,7 +2,6 @@
 
 use logid::{
     id_map::LogIdMap,
-    log_id::{self, EventLevel},
 };
 use once_cell::sync::Lazy;
 
@@ -21,37 +20,6 @@ pub enum LogIdMainGrp {
     Backend = 3,
     /// Element log-id sub group
     Element = 4,
-    /// Config log-id sub group
-    Config = 5,
     /// Security log-id sub group
     Security = 6,
-}
-
-enum LogIdSubGrp {
-    General = 0,
-}
-
-/// Log-ids for config errors
-pub enum ConfigErrLogId {
-    /// Log-id denoting an invalid file in the config
-    InvalidFile = log_id::get_log_id(
-        LogIdMainGrp::Config as u8,
-        LogIdSubGrp::General as u8,
-        EventLevel::Error,
-        0,
-    ),
-    /// Log-id denoting an invalid path in the config
-    InvalidPath = log_id::get_log_id(
-        LogIdMainGrp::Config as u8,
-        LogIdSubGrp::General as u8,
-        EventLevel::Error,
-        1,
-    ),
-    /// Log-id denoting an invalid config
-    InvalidConfig = log_id::get_log_id(
-        LogIdMainGrp::Config as u8,
-        LogIdSubGrp::General as u8,
-        EventLevel::Error,
-        2,
-    ),
 }
