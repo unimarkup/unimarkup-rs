5a74b6b041ba6a900ecd4ba9d473716c04bbf513
Manuel Hatzl <hatzlmanuel@outlook.com>
2023-06-05 22:48:44 +0200

    feat!: force rendering to never fail
    Render trait functions now return the rendered output directly.

--- render/src/html.rs
+++ render/src/html.rs
@@ -1,6 +1,6 @@
 //! Defines the [`Html`] struct that is returned by the [`Render`](crate::render::Render) trait when rendering Unimarkup to HTML.
 
-use crate::render::{RenderBlock, RenderErr};
+use crate::render::RenderBlock;
 
 #[derive(Debug, Default)]
 pub struct Html {
@@ -13,17 +13,14 @@ pub struct Html {
 /// # Arguments
 ///
 /// - `blocks` - array of type [`RenderBlock`]
-///
-/// Returns a [`RenderErr`], if any of the given blocks return a [`MappedLogId`]
-/// when rendering themself.
-pub fn render_html(blocks: &[RenderBlock]) -> Result<Html, RenderErr> {
+pub fn render_html(blocks: &[RenderBlock]) -> Html {
     let mut html = Html::default();
 
     for block in blocks {
-        let html_part = block.render_html()?;
+        let html_part = block.render_html();
         html.body.push_str(&html_part.body);
         html.head.push_str(&html_part.head);
     }
 
-    Ok(html)
+    html
 }
--- render/src/render.rs
+++ render/src/render.rs
@@ -7,26 +7,8 @@ pub type RenderBlock = Box<dyn Render>;
 
 /// Trait to provide render implementation for supported output formats
 pub trait Render {
+    // Note: Rendering must not fail => Type directly instead of Result or Option
+
     /// Generates the HTML representation of an Unimarkup element.
-    ///
-    /// Returns a [`RenderErr`] if it's not possible to generate valid HTML, i.e. if
-    /// provided id contains characters not allowed in HTML id attribute.
-    fn render_html(&self) -> Result<Html, RenderErr>;
-}
-
-#[derive(Debug, Clone)]
-pub struct RenderErr {
-    cause: logid::evident::event::intermediary::FinalizedEvent<logid::log_id::LogId>,
-}
-
-impl std::error::Error for RenderErr {}
-
-impl std::fmt::Display for RenderErr {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "eventId='{}', entryId='{}'",
-            self.cause.event_id, self.cause.entry_id
-        )
-    }
+    fn render_html(&self) -> Html;
 }
