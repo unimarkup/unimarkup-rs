da684316464a4d37704dd186fc94a8301fd1c371
Nadir Fejzic <nadirfejzo@gmail.com>
2023-06-08 21:50:49 +0200

    feat: implement Scanner with localisation support

--- commons/src/scanner/mod.rs
+++ commons/src/scanner/mod.rs
@@ -4,59 +4,105 @@ pub mod position;
 pub mod span;
 mod symbol;
 
-use icu_segmenter::GraphemeClusterSegmenter;
+use icu_provider::{AnyProvider, BufferProvider};
+use icu_segmenter::{GraphemeClusterSegmenter, SegmenterError};
 use position::{Offset, Position};
-pub use symbol::{IntoSymbols, Symbol, SymbolKind};
+pub use symbol::{Symbol, SymbolKind};
 
-// TODO: pass locale from Config to this function.
-fn grapheme_split(input: &str) -> Vec<Symbol> {
-    let segmenter =
-        GraphemeClusterSegmenter::try_new_unstable(&icu_testdata::unstable()).expect("Data exists");
+#[derive(Debug)]
+pub struct Scanner<P> {
+    provider: P,
+    segmenter: GraphemeClusterSegmenter,
+}
 
-    let mut symbols: Vec<Symbol> = Vec::new();
-    let mut curr_pos: Position = Position::default();
-    let mut prev_offset = 0;
+impl<P> Scanner<P> {
+    pub fn try_new(provider: P) -> Result<Self, SegmenterError>
+    where
+        P: BufferProvider,
+    {
+        let segmenter = GraphemeClusterSegmenter::try_new_with_buffer_provider(&provider)?;
 
-    // skip(1) to ignore break at start of input
-    for offset in segmenter.segment_str(input).skip(1) {
-        if let Some(grapheme) = input.get(prev_offset..offset) {
-            let mut kind = SymbolKind::from(grapheme);
-
-            let end_pos = if kind == SymbolKind::Newline {
-                Position {
-                    line: (curr_pos.line + 1),
-                    ..Default::default()
-                }
-            } else {
-                Position {
-                    line: curr_pos.line,
-                    col_utf8: (curr_pos.col_utf8 + grapheme.len()),
-                    col_utf16: (curr_pos.col_utf16 + grapheme.encode_utf16().count()),
-                    col_grapheme: (curr_pos.col_grapheme + 1),
-                }
-            };
-
-            if curr_pos.col_utf8 == 1 && kind == SymbolKind::Newline {
-                // newline at the start of line -> Blankline
-                kind = SymbolKind::Blankline;
-            }
-
-            symbols.push(Symbol {
-                input,
-                kind,
-                offset: Offset {
-                    start: prev_offset,
-                    end: offset,
-                },
-                start: curr_pos,
-                end: end_pos,
-            });
-
-            curr_pos = end_pos;
-        }
-        prev_offset = offset;
+        Ok(Self {
+            provider,
+            segmenter,
+        })
     }
 
-    // last offset not needed, because break at EOI is always available
-    symbols
+    pub fn try_new_with_any(provider: P) -> Result<Self, SegmenterError>
+    where
+        P: AnyProvider,
+    {
+        let segmenter = GraphemeClusterSegmenter::try_new_with_any_provider(&provider)?;
+
+        Ok(Self {
+            provider,
+            segmenter,
+        })
+    }
+}
+
+impl<P> Clone for Scanner<P>
+where
+    P: Clone + BufferProvider,
+{
+    fn clone(&self) -> Self {
+        let segmenter = GraphemeClusterSegmenter::try_new_with_buffer_provider(&self.provider)
+            .expect("Provider is valid at this point.");
+
+        Self {
+            provider: self.provider.clone(),
+            segmenter,
+        }
+    }
+}
+
+impl<P> Scanner<P> {
+    pub fn scan_str<'s>(&self, input: &'s str) -> Vec<Symbol<'s>> {
+        let mut symbols: Vec<Symbol> = Vec::new();
+        let mut curr_pos: Position = Position::default();
+        let mut prev_offset = 0;
+
+        // skip(1) to ignore break at start of input
+        for offset in self.segmenter.segment_str(input).skip(1) {
+            if let Some(grapheme) = input.get(prev_offset..offset) {
+                let mut kind = SymbolKind::from(grapheme);
+
+                let end_pos = if kind == SymbolKind::Newline {
+                    Position {
+                        line: (curr_pos.line + 1),
+                        ..Default::default()
+                    }
+                } else {
+                    Position {
+                        line: curr_pos.line,
+                        col_utf8: (curr_pos.col_utf8 + grapheme.len()),
+                        col_utf16: (curr_pos.col_utf16 + grapheme.encode_utf16().count()),
+                        col_grapheme: (curr_pos.col_grapheme + 1),
+                    }
+                };
+
+                if curr_pos.col_utf8 == 1 && kind == SymbolKind::Newline {
+                    // newline at the start of line -> Blankline
+                    kind = SymbolKind::Blankline;
+                }
+
+                symbols.push(Symbol {
+                    input,
+                    kind,
+                    offset: Offset {
+                        start: prev_offset,
+                        end: offset,
+                    },
+                    start: curr_pos,
+                    end: end_pos,
+                });
+
+                curr_pos = end_pos;
+            }
+            prev_offset = offset;
+        }
+
+        // last offset not needed, because break at EOI is always available
+        symbols
+    }
 }
